e28f4d77ccdf2be8d03ee1b3b4a5973ec2d1d150
MousumiPuja <39323943+MousumiPuja@users.noreply.github.com>
2019-06-17 09:35:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 /* Task 1 */
 
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
+heading.innerText= "This is new"
+
 
 /* Task 2 using plain JavaScript */
+//Task 2a
+var textBtn = document.getElementById('addText1')
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "Hello World"
+    task2.appendChild(paragraph)
+})
 
+//Task 2b
+function changeBGColor(event){
+    //console.log(event.target.innerText)
+    var body = document.getElementsByTagName('body')[0]
+    body.style.backgroundColor = event.target.innerText.toLowerCase()
+}
+
+// Task 2c
+function redirectToGoogle(event){
+    window.location.assign("https://www.google.com/")
+} 
 
 
 /* Task 4 using jQuery */
+
+//Task- 4a
+$('#addText2').click(function(event){
+   // console.log("clicked")
+    var paragraph= $('<p>').text('Hello World')
+    $('#task4a').append(paragraph)
+})
+//Task- 4b
+function changeBG(event){
+    var color= event.target.innerText.toLowerCase()
+    $('body').css('background-color', color)
+}
+//Task- 4c
+$('#add').click(function(event){
+var sum = Number($('#num1').val())+Number($('#num2').val())
+$('#sum').text(sum)
+})
